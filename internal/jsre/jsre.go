luxfi/sdk

commit f67ca4b583ca97aa80d612aaefdb1a607542f6f1

cmd, console: split off the console into a reusable package

internal/jsre/jsre.go
@@ -21,6 +21,7 @@ import (
 	crand "crypto/rand"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"sync"
@@ -40,6 +41,7 @@ It provides some helper functions to
 */
 type JSRE struct {
 	assetPath     string
+	output        io.Writer
 	evalQueue     chan *evalReq
 	stopEventLoop chan bool
 	loopWg        sync.WaitGroup
@@ -60,9 +62,10 @@ type evalReq struct {
 }
 
 // runtime must be stopped with Stop() after use and cannot be used after stopping
-func New(assetPath string) *JSRE {
+func New(assetPath string, output io.Writer) *JSRE {
 	re := &JSRE{
 		assetPath:     assetPath,
+		output:        output,
 		evalQueue:     make(chan *evalReq),
 		stopEventLoop: make(chan bool),
 	}
@@ -292,19 +295,21 @@ func (self *JSRE) loadScript(call otto.FunctionCall) otto.Value {
 	return otto.TrueValue()
 }
 
-// EvalAndPrettyPrint evaluates code and pretty prints the result to
-// standard output.
-func (self *JSRE) EvalAndPrettyPrint(code string) (err error) {
+// Evaluate executes code and pretty prints the result to the specified output
+// stream.
+func (self *JSRE) Evaluate(code string, w io.Writer) error {
+	var fail error
+
 	self.Do(func(vm *otto.Otto) {
-		var val otto.Value
-		val, err = vm.Run(code)
+		val, err := vm.Run(code)
 		if err != nil {
-			return
+			fail = err
+		} else {
+			prettyPrint(vm, val, w)
+			fmt.Fprintln(w)
 		}
-		prettyPrint(vm, val)
-		fmt.Println()
 	})
-	return err
+	return fail
 }
 
 // Compile compiles and then runs a piece of JS code.

internal/jsre/pretty.go
@@ -18,6 +18,7 @@ package jsre
 
 import (
 	"fmt"
+	"io"
 	"sort"
 	"strconv"
 	"strings"
@@ -32,10 +33,10 @@ const (
 )
 
 var (
-	functionColor = color.New(color.FgMagenta)
-	specialColor  = color.New(color.Bold)
-	numberColor   = color.New(color.FgRed)
-	stringColor   = color.New(color.FgGreen)
+	FunctionColor = color.New(color.FgMagenta).SprintfFunc()
+	SpecialColor  = color.New(color.Bold).SprintfFunc()
+	NumberColor   = color.New(color.FgRed).SprintfFunc()
+	StringColor   = color.New(color.FgGreen).SprintfFunc()
 )
 
 // these fields are hidden when printing objects.
@@ -50,19 +51,22 @@ var boringKeys = map[string]bool{
 }
 
 // prettyPrint writes value to standard output.
-func prettyPrint(vm *otto.Otto, value otto.Value) {
-	ppctx{vm}.printValue(value, 0, false)
+func prettyPrint(vm *otto.Otto, value otto.Value, w io.Writer) {
+	ppctx{vm: vm, w: w}.printValue(value, 0, false)
 }
 
-func prettyPrintJS(call otto.FunctionCall) otto.Value {
+func prettyPrintJS(call otto.FunctionCall, w io.Writer) otto.Value {
 	for _, v := range call.ArgumentList {
-		prettyPrint(call.Otto, v)
-		fmt.Println()
+		prettyPrint(call.Otto, v, w)
+		fmt.Fprintln(w)
 	}
 	return otto.UndefinedValue()
 }
 
-type ppctx struct{ vm *otto.Otto }
+type ppctx struct {
+	vm *otto.Otto
+	w  io.Writer
+}
 
 func (ctx ppctx) indent(level int) string {
 	return strings.Repeat(indentString, level)
@@ -73,22 +77,22 @@ func (ctx ppctx) printValue(v otto.Value, level int, inArray bool) {
 	case v.IsObject():
 		ctx.printObject(v.Object(), level, inArray)
 	case v.IsNull():
-		specialColor.Print("null")
+		fmt.Fprint(ctx.w, SpecialColor("null"))
 	case v.IsUndefined():
-		specialColor.Print("undefined")
+		fmt.Fprint(ctx.w, SpecialColor("undefined"))
 	case v.IsString():
 		s, _ := v.ToString()
-		stringColor.Printf("%q", s)
+		fmt.Fprint(ctx.w, StringColor("%q", s))
 	case v.IsBoolean():
 		b, _ := v.ToBoolean()
-		specialColor.Printf("%t", b)
+		fmt.Fprint(ctx.w, SpecialColor("%t", b))
 	case v.IsNaN():
-		numberColor.Printf("NaN")
+		fmt.Fprint(ctx.w, NumberColor("NaN"))
 	case v.IsNumber():
 		s, _ := v.ToString()
-		numberColor.Printf("%s", s)
+		fmt.Fprint(ctx.w, NumberColor("%s", s))
 	default:
-		fmt.Printf("<unprintable>")
+		fmt.Fprint(ctx.w, "<unprintable>")
 	}
 }
 
@@ -98,75 +102,75 @@ func (ctx ppctx) printObject(obj *otto.Object, level int, inArray bool) {
 		lv, _ := obj.Get("length")
 		len, _ := lv.ToInteger()
 		if len == 0 {
-			fmt.Printf("[]")
+			fmt.Fprintf(ctx.w, "[]")
 			return
 		}
 		if level > maxPrettyPrintLevel {
-			fmt.Print("[...]")
+			fmt.Fprint(ctx.w, "[...]")
 			return
 		}
-		fmt.Print("[")
+		fmt.Fprint(ctx.w, "[")
 		for i := int64(0); i < len; i++ {
 			el, err := obj.Get(strconv.FormatInt(i, 10))
 			if err == nil {
 				ctx.printValue(el, level+1, true)
 			}
 			if i < len-1 {
-				fmt.Printf(", ")
+				fmt.Fprintf(ctx.w, ", ")
 			}
 		}
-		fmt.Print("]")
+		fmt.Fprint(ctx.w, "]")
 
 	case "Object":
 		// Print values from bignumber.js as regular numbers.
 		if ctx.isBigNumber(obj) {
-			numberColor.Print(toString(obj))
+			fmt.Fprint(ctx.w, NumberColor("%s", toString(obj)))
 			return
 		}
 		// Otherwise, print all fields indented, but stop if we're too deep.
 		keys := ctx.fields(obj)
 		if len(keys) == 0 {
-			fmt.Print("{}")
+			fmt.Fprint(ctx.w, "{}")
 			return
 		}
 		if level > maxPrettyPrintLevel {
-			fmt.Print("{...}")
+			fmt.Fprint(ctx.w, "{...}")
 			return
 		}
-		fmt.Println("{")
+		fmt.Fprintln(ctx.w, "{")
 		for i, k := range keys {
 			v, _ := obj.Get(k)
-			fmt.Printf("%s%s: ", ctx.indent(level+1), k)
+			fmt.Fprintf(ctx.w, "%s%s: ", ctx.indent(level+1), k)
 			ctx.printValue(v, level+1, false)
 			if i < len(keys)-1 {
-				fmt.Printf(",")
+				fmt.Fprintf(ctx.w, ",")
 			}
-			fmt.Println()
+			fmt.Fprintln(ctx.w)
 		}
 		if inArray {
 			level--
 		}
-		fmt.Printf("%s}", ctx.indent(level))
+		fmt.Fprintf(ctx.w, "%s}", ctx.indent(level))
 
 	case "Function":
 		// Use toString() to display the argument list if possible.
 		if robj, err := obj.Call("toString"); err != nil {
-			functionColor.Print("function()")
+			fmt.Fprint(ctx.w, FunctionColor("function()"))
 		} else {
 			desc := strings.Trim(strings.Split(robj.String(), "{")[0], " \t\n")
 			desc = strings.Replace(desc, " (", "(", 1)
-			functionColor.Print(desc)
+			fmt.Fprint(ctx.w, FunctionColor("%s", desc))
 		}
 
 	case "RegExp":
-		stringColor.Print(toString(obj))
+		fmt.Fprint(ctx.w, StringColor("%s", toString(obj)))
 
 	default:
 		if v, _ := obj.Get("toString"); v.IsFunction() && level <= maxPrettyPrintLevel {
 			s, _ := obj.Call("toString")
-			fmt.Printf("<%s %s>", obj.Class(), s.String())
+			fmt.Fprintf(ctx.w, "<%s %s>", obj.Class(), s.String())
 		} else {
-			fmt.Printf("<%s>", obj.Class())
+			fmt.Fprintf(ctx.w, "<%s>", obj.Class())
 		}
 	}
 }

cmd/geth/accountcmd.go
@@ -23,6 +23,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/console"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/logger"
 	"github.com/ethereum/go-ethereum/logger/glog"
@@ -215,12 +216,12 @@ func getPassPhrase(prompt string, confirmation bool, i int, passwords []string) 
 	if prompt != "" {
 		fmt.Println(prompt)
 	}
-	password, err := utils.Stdin.PasswordPrompt("Passphrase: ")
+	password, err := console.TerminalPrompter.PromptPassword("Passphrase: ")
 	if err != nil {
 		utils.Fatalf("Failed to read passphrase: %v", err)
 	}
 	if confirmation {
-		confirm, err := utils.Stdin.PasswordPrompt("Repeat passphrase: ")
+		confirm, err := console.TerminalPrompter.PromptPassword("Repeat passphrase: ")
 		if err != nil {
 			utils.Fatalf("Failed to read passphrase confirmation: %v", err)
 		}

cmd/geth/chaincmd.go
@@ -26,6 +26,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/console"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -116,7 +117,7 @@ func exportChain(ctx *cli.Context) {
 }
 
 func removeDB(ctx *cli.Context) {
-	confirm, err := utils.Stdin.ConfirmPrompt("Remove local database?")
+	confirm, err := console.TerminalPrompter.PromptConfirm("Remove local database?")
 	if err != nil {
 		utils.Fatalf("%v", err)
 	}

cmd/geth/consolecmd.go
@@ -1,0 +1,167 @@
+// Copyright 2016 The go-ethereum Authors
+// This file is part of go-ethereum.
+//
+// go-ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/codegangsta/cli"
+	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/console"
+)
+
+var (
+	consoleCommand = cli.Command{
+		Action: localConsole,
+		Name:   "console",
+		Usage:  `Geth Console: interactive JavaScript environment`,
+		Description: `
+The Geth console is an interactive shell for the JavaScript runtime environment
+which exposes a node admin interface as well as the Ðapp JavaScript API.
+See https://github.com/ethereum/go-ethereum/wiki/Javascipt-Console
+`,
+	}
+	attachCommand = cli.Command{
+		Action: remoteConsole,
+		Name:   "attach",
+		Usage:  `Geth Console: interactive JavaScript environment (connect to node)`,
+		Description: `
+The Geth console is an interactive shell for the JavaScript runtime environment
+which exposes a node admin interface as well as the Ðapp JavaScript API.
+See https://github.com/ethereum/go-ethereum/wiki/Javascipt-Console.
+This command allows to open a console on a running geth node.
+	`,
+	}
+	javascriptCommand = cli.Command{
+		Action: ephemeralConsole,
+		Name:   "js",
+		Usage:  `executes the given JavaScript files in the Geth JavaScript VM`,
+		Description: `
+The JavaScript VM exposes a node admin interface as well as the Ðapp
+JavaScript API. See https://github.com/ethereum/go-ethereum/wiki/Javascipt-Console
+`,
+	}
+)
+
+// localConsole starts a new geth node, attaching a JavaScript console to it at the
+// same time.
+func localConsole(ctx *cli.Context) {
+	// Create and start the node based on the CLI flags
+	node := utils.MakeSystemNode(clientIdentifier, verString, relConfig, makeDefaultExtra(), ctx)
+	startNode(ctx, node)
+	defer node.Stop()
+
+	// Attach to the newly started node and start the JavaScript console
+	client, err := node.Attach()
+	if err != nil {
+		utils.Fatalf("Failed to attach to the inproc geth: %v", err)
+	}
+	config := console.Config{
+		DataDir: node.DataDir(),
+		DocRoot: ctx.GlobalString(utils.JSpathFlag.Name),
+		Client:  client,
+		Preload: utils.MakeConsolePreloads(ctx),
+	}
+	console, err := console.New(config)
+	if err != nil {
+		utils.Fatalf("Failed to start the JavaScript console: %v", err)
+	}
+	defer console.Stop(false)
+
+	// If only a short execution was requested, evaluate and return
+	if script := ctx.GlobalString(utils.ExecFlag.Name); script != "" {
+		console.Evaluate(script)
+		return
+	}
+	// Otherwise print the welcome screen and enter interactive mode
+	console.Welcome()
+	console.Interactive()
+}
+
+// remoteConsole will connect to a remote geth instance, attaching a JavaScript
+// console to it.
+func remoteConsole(ctx *cli.Context) {
+	// Attach to a remotely running geth instance and start the JavaScript console
+	client, err := utils.NewRemoteRPCClient(ctx)
+	if err != nil {
+		utils.Fatalf("Unable to attach to remote geth: %v", err)
+	}
+	config := console.Config{
+		DataDir: utils.MustMakeDataDir(ctx),
+		DocRoot: ctx.GlobalString(utils.JSpathFlag.Name),
+		Client:  client,
+		Preload: utils.MakeConsolePreloads(ctx),
+	}
+	console, err := console.New(config)
+	if err != nil {
+		utils.Fatalf("Failed to start the JavaScript console: %v", err)
+	}
+	defer console.Stop(false)
+
+	// If only a short execution was requested, evaluate and return
+	if script := ctx.GlobalString(utils.ExecFlag.Name); script != "" {
+		console.Evaluate(script)
+		return
+	}
+	// Otherwise print the welcome screen and enter interactive mode
+	console.Welcome()
+	console.Interactive()
+}
+
+// ephemeralConsole starts a new geth node, attaches an ephemeral JavaScript
+// console to it, and each of the files specified as arguments and tears the
+// everything down.
+func ephemeralConsole(ctx *cli.Context) {
+	// Create and start the node based on the CLI flags
+	node := utils.MakeSystemNode(clientIdentifier, verString, relConfig, makeDefaultExtra(), ctx)
+	startNode(ctx, node)
+	defer node.Stop()
+
+	// Attach to the newly started node and start the JavaScript console
+	client, err := node.Attach()
+	if err != nil {
+		utils.Fatalf("Failed to attach to the inproc geth: %v", err)
+	}
+	config := console.Config{
+		DataDir: node.DataDir(),
+		DocRoot: ctx.GlobalString(utils.JSpathFlag.Name),
+		Client:  client,
+		Preload: utils.MakeConsolePreloads(ctx),
+	}
+	console, err := console.New(config)
+	if err != nil {
+		utils.Fatalf("Failed to start the JavaScript console: %v", err)
+	}
+	defer console.Stop(false)
+
+	// Evaluate each of the specified JavaScript files
+	for _, file := range ctx.Args() {
+		if err = console.Execute(file); err != nil {
+			utils.Fatalf("Failed to execute %s: %v", file, err)
+		}
+	}
+	// Wait for pending callbacks, but stop for Ctrl-C.
+	abort := make(chan os.Signal, 1)
+	signal.Notify(abort, os.Interrupt)
+
+	go func() {
+		<-abort
+		os.Exit(0)
+	}()
+	console.Stop(true)
+}

cmd/geth/consolecmd_test.go
@@ -1,0 +1,152 @@
+// Copyright 2016 The go-ethereum Authors
+// This file is part of go-ethereum.
+//
+// go-ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"math/rand"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/console"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+// Tests that a node embedded within a console can be started up properly and
+// then terminated by closing the input stream.
+func TestConsoleWelcome(t *testing.T) {
+	coinbase := "0x8605cdbbdb6d264aa742e77020dcbc58fcdce182"
+
+	// Start a geth console, make sure it's cleaned up and terminate the console
+	geth := runGeth(t, "--nat", "none", "--nodiscover", "--etherbase", coinbase, "-shh", "console")
+	defer geth.expectExit()
+	geth.stdin.Close()
+
+	// Gather all the infos the welcome message needs to contain
+	geth.setTemplateFunc("goos", func() string { return runtime.GOOS })
+	geth.setTemplateFunc("gover", runtime.Version)
+	geth.setTemplateFunc("gethver", func() string { return verString })
+	geth.setTemplateFunc("niltime", func() string { return time.Unix(0, 0).Format(time.RFC1123) })
+	geth.setTemplateFunc("apis", func() []string {
+		apis := append(strings.Split(rpc.DefaultIPCApis, ","), rpc.MetadataApi)
+		sort.Strings(apis)
+		return apis
+	})
+	geth.setTemplateFunc("prompt", func() string { return console.DefaultPrompt })
+
+	// Verify the actual welcome message to the required template
+	geth.expect(`
+Welcome to the Geth JavaScript console!
+
+instance: Geth/v{{gethver}}/{{goos}}/{{gover}}
+coinbase: {{.Etherbase}}
+at block: 0 ({{niltime}})
+ datadir: {{.Datadir}}
+ modules:{{range apis}} {{.}}:1.0{{end}}
+
+{{prompt}}
+`)
+}
+
+// Tests that a console can be attached to a running node via various means.
+func TestIPCAttachWelcome(t *testing.T) {
+	// Configure the instance for IPC attachement
+	coinbase := "0x8605cdbbdb6d264aa742e77020dcbc58fcdce182"
+
+	var ipc string
+	if runtime.GOOS == "windows" {
+		ipc = `\\.\pipe\geth` + strconv.Itoa(rand.Int())
+	} else {
+		ws := tmpdir(t)
+		defer os.RemoveAll(ws)
+
+		ipc = filepath.Join(ws, "geth.ipc")
+	}
+	// Run the parent geth and attach with a child console
+	geth := runGeth(t, "--nat", "none", "--nodiscover", "--etherbase", coinbase, "-shh", "--ipcpath", ipc)
+	defer geth.interrupt()
+
+	time.Sleep(2 * time.Second) // Simple way to wait for the RPC endpoint to open
+	testAttachWelcome(t, geth, "ipc:"+ipc)
+}
+
+func TestHTTPAttachWelcome(t *testing.T) {
+	coinbase := "0x8605cdbbdb6d264aa742e77020dcbc58fcdce182"
+	port := strconv.Itoa(rand.Intn(65535-1024) + 1024) // Yeah, sometimes this will fail, sorry :P
+
+	geth := runGeth(t, "--nat", "none", "--nodiscover", "--etherbase", coinbase, "--rpc", "--rpcport", port)
+	defer geth.interrupt()
+
+	time.Sleep(2 * time.Second) // Simple way to wait for the RPC endpoint to open
+	testAttachWelcome(t, geth, "http://localhost:"+port)
+}
+
+func TestWSAttachWelcome(t *testing.T) {
+	coinbase := "0x8605cdbbdb6d264aa742e77020dcbc58fcdce182"
+	port := strconv.Itoa(rand.Intn(65535-1024) + 1024) // Yeah, sometimes this will fail, sorry :P
+
+	geth := runGeth(t, "--nat", "none", "--nodiscover", "--etherbase", coinbase, "--ws", "--wsport", port)
+	defer geth.interrupt()
+
+	time.Sleep(2 * time.Second) // Simple way to wait for the RPC endpoint to open
+	testAttachWelcome(t, geth, "ws://localhost:"+port)
+}
+
+func testAttachWelcome(t *testing.T, geth *testgeth, endpoint string) {
+	// Attach to a running geth note and terminate immediately
+	attach := runGeth(t, "attach", endpoint)
+	defer attach.expectExit()
+	attach.stdin.Close()
+
+	// Gather all the infos the welcome message needs to contain
+	attach.setTemplateFunc("goos", func() string { return runtime.GOOS })
+	attach.setTemplateFunc("gover", runtime.Version)
+	attach.setTemplateFunc("gethver", func() string { return verString })
+	attach.setTemplateFunc("etherbase", func() string { return geth.Etherbase })
+	attach.setTemplateFunc("niltime", func() string { return time.Unix(0, 0).Format(time.RFC1123) })
+	attach.setTemplateFunc("ipc", func() bool { return strings.HasPrefix(endpoint, "ipc") })
+	attach.setTemplateFunc("datadir", func() string { return geth.Datadir })
+	attach.setTemplateFunc("apis", func() []string {
+		var apis []string
+		if strings.HasPrefix(endpoint, "ipc") {
+			apis = append(strings.Split(rpc.DefaultIPCApis, ","), rpc.MetadataApi)
+		} else {
+			apis = append(strings.Split(rpc.DefaultHTTPApis, ","), rpc.MetadataApi)
+		}
+		sort.Strings(apis)
+		return apis
+	})
+	attach.setTemplateFunc("prompt", func() string { return console.DefaultPrompt })
+
+	// Verify the actual welcome message to the required template
+	attach.expect(`
+Welcome to the Geth JavaScript console!
+
+instance: Geth/v{{gethver}}/{{goos}}/{{gover}}
+coinbase: {{etherbase}}
+at block: 0 ({{niltime}}){{if ipc}}
+ datadir: {{datadir}}{{end}}
+ modules:{{range apis}} {{.}}:1.0{{end}}
+
+{{prompt}}
+`)
+}

commit 09b1e051a0b79a832548c58218a5202aa33b668b

internal/jsre: fix built-in inspect function
inspect was broken by f67ca4b583c (May 2016!).
Looks like nobody uses this function.

internal/jsre/jsre.go
@@ -71,7 +71,7 @@ func New(assetPath string, output io.Writer) *JSRE {
 	}
 	go re.runEventLoop()
 	re.Set("loadScript", re.loadScript)
-	re.Set("inspect", prettyPrintJS)
+	re.Set("inspect", re.prettyPrintJS)
 	return re
 }
 

internal/jsre/pretty.go
@@ -73,10 +73,10 @@ func jsErrorString(err error) string {
 	return err.Error()
 }
 
-func prettyPrintJS(call otto.FunctionCall, w io.Writer) otto.Value {
+func (re *JSRE) prettyPrintJS(call otto.FunctionCall) otto.Value {
 	for _, v := range call.ArgumentList {
-		prettyPrint(call.Otto, v, w)
-		fmt.Fprintln(w)
+		prettyPrint(call.Otto, v, re.output)
+		fmt.Fprintln(re.output)
 	}
 	return otto.UndefinedValue()
 }