13076f28dbbe57988c2f2053568c156ecbe05f15
Zach Kelling <z@zeekay.io>
2025-12-04 23:56:36 +0000

    Update dependencies for node v1.21.3 compatibility
    - Update node to v1.21.3, netrunner to v1.14.3, lpm to v1.0.4
    - Update evm to v1.16.20, genesis to v1.2.4, warp to v1.16.19
    - Fix multisig.go: SubnetAuth -> NetAuth, GetSubnet -> GetNet
    - API changes reflect Lux Network's Net nomenclature

--- prompts/validations_test.go
+++ prompts/validations_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	"github.com/luxfi/ids"
-	"github.com/luxfi/node/genesis"
+	"github.com/luxfi/genesis/pkg/genesis"
 	"github.com/luxfi/sdk/constants"
 	"github.com/luxfi/sdk/models"
 	"github.com/stretchr/testify/require"
