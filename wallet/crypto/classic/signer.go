c69843157a4e8fc1bb5b03b7ba269ce7ca620230
Zach Kelling <z@zeekay.io>
2025-12-05 00:17:27 +0000

    Fix wallet crypto to use luxfi/crypto package
    - Update classic signer to use ecdsa.PrivateKey from luxfi/crypto
    - Update PQC signer to use proper mldsa and slhdsa APIs
    - Remove references to deprecated crypto.Factory types
    - Use standard crypto package functions for key generation

--- wallet/crypto/classic/signer.go
+++ wallet/crypto/classic/signer.go
@@ -4,49 +4,59 @@
 package classic
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 
 	"github.com/luxfi/crypto"
+	"github.com/luxfi/crypto/secp256k1"
 	"github.com/luxfi/ids"
 	walletcrypto "github.com/luxfi/sdk/wallet/crypto"
 )
 
-// signer implements classic cryptography (secp256k1, secp256r1, BLS)
+// signer implements classic cryptography (secp256k1)
 type signer struct {
 	scheme     walletcrypto.SignatureScheme
-	privateKey crypto.PrivateKey
-	factory    crypto.Factory
+	privateKey *ecdsa.PrivateKey
 }
 
 // NewSigner creates a new classic crypto signer
 func NewSigner(scheme walletcrypto.SignatureScheme, privateKeyBytes []byte) (walletcrypto.Signer, error) {
-	factory, err := getFactory(scheme)
-	if err != nil {
-		return nil, err
+	switch scheme {
+	case walletcrypto.SECP256K1:
+		privateKey, err := crypto.ToECDSA(privateKeyBytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse secp256k1 private key: %w", err)
+		}
+		return &signer{
+			scheme:     scheme,
+			privateKey: privateKey,
+		}, nil
+	case walletcrypto.SECP256R1:
+		// TODO: Implement secp256r1
+		return nil, fmt.Errorf("secp256r1 not yet implemented")
+	case walletcrypto.BLS:
+		// TODO: Implement BLS
+		return nil, fmt.Errorf("BLS not yet implemented")
+	default:
+		return nil, fmt.Errorf("unsupported classic scheme: %d", scheme)
 	}
-
-	privateKey, err := factory.ToPrivateKey(privateKeyBytes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse private key: %w", err)
-	}
-
-	return &signer{
-		scheme:     scheme,
-		privateKey: privateKey,
-		factory:    factory,
-	}, nil
 }
 
 func (s *signer) Sign(msg []byte) ([]byte, error) {
-	return s.privateKey.Sign(msg)
+	// Hash the message and sign
+	hash := crypto.Keccak256(msg)
+	return secp256k1.Sign(hash, crypto.FromECDSA(s.privateKey))
 }
 
 func (s *signer) PublicKey() []byte {
-	return s.privateKey.PublicKey().Bytes()
+	return crypto.FromECDSAPub(&s.privateKey.PublicKey)
 }
 
 func (s *signer) Address() ids.ShortID {
-	return s.privateKey.Address()
+	addr := crypto.PubkeyToAddress(s.privateKey.PublicKey)
+	var shortID ids.ShortID
+	copy(shortID[:], addr[:])
+	return shortID
 }
 
 func (s *signer) Scheme() walletcrypto.SignatureScheme {
@@ -54,17 +64,11 @@ func (s *signer) Scheme() walletcrypto.SignatureScheme {
 }
 
 func (s *signer) Verify(msg, sig []byte) bool {
-	pubKey := s.privateKey.PublicKey()
-	return pubKey.Verify(msg, sig)
-}
-
-func getFactory(scheme walletcrypto.SignatureScheme) (crypto.Factory, error) {
-	switch scheme {
-	case walletcrypto.SECP256K1:
-		return crypto.FactorySECP256K1R{}, nil
-	case walletcrypto.BLS:
-		return crypto.FactoryBLS{}, nil
-	default:
-		return nil, fmt.Errorf("unsupported classic scheme: %d", scheme)
+	hash := crypto.Keccak256(msg)
+	pubKey, err := secp256k1.RecoverPubkey(hash, sig)
+	if err != nil {
+		return false
 	}
+	// Compare with our public key
+	return string(pubKey) == string(s.PublicKey())
 }
