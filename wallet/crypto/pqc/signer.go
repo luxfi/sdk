33285ce83dda2ebf1a2671f3bf2c17ca2a998b5a
Zach Kelling <z@zeekay.io>
2025-12-05 00:17:27 +0000

    Fix wallet crypto to use luxfi/crypto package
    - Update classic signer to use ecdsa.PrivateKey from luxfi/crypto
    - Update PQC signer to use proper mldsa and slhdsa APIs
    - Remove references to deprecated crypto.Factory types
    - Use standard crypto package functions for key generation

--- wallet/crypto/pqc/signer.go
+++ wallet/crypto/pqc/signer.go
@@ -4,63 +4,103 @@
 package pqc
 
 import (
+	"crypto/rand"
 	"fmt"
 
 	"github.com/luxfi/crypto/mldsa"
-	"github.com/luxfi/crypto/mlkem"
 	"github.com/luxfi/crypto/slhdsa"
 	"github.com/luxfi/ids"
 	walletcrypto "github.com/luxfi/sdk/wallet/crypto"
 )
 
-// signer implements post-quantum cryptography
+// signer implements post-quantum cryptography using ML-DSA and SLH-DSA
 type signer struct {
-	scheme     walletcrypto.SignatureScheme
-	privateKey []byte
-	publicKey  []byte
-	address    ids.ShortID
+	scheme      walletcrypto.SignatureScheme
+	mldsaKey    *mldsa.PrivateKey
+	slhdsaKey   *slhdsa.PrivateKey
+	publicKey   []byte
+	address     ids.ShortID
 }
 
 // NewSigner creates a new PQC signer
 func NewSigner(scheme walletcrypto.SignatureScheme, privateKeyBytes []byte) (walletcrypto.Signer, error) {
 	s := &signer{
-		scheme:     scheme,
-		privateKey: privateKeyBytes,
+		scheme: scheme,
 	}
 
-	// Generate public key and address based on scheme
+	// Generate or load keys based on scheme
 	switch scheme {
 	case walletcrypto.MLDSA87:
-		pubKey, err := mldsa.GeneratePublicKey87(privateKeyBytes)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate ML-DSA-87 public key: %w", err)
+		if len(privateKeyBytes) > 0 {
+			privKey, err := mldsa.PrivateKeyFromBytes(mldsa.MLDSA87, privateKeyBytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load ML-DSA-87 private key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
+		} else {
+			privKey, err := mldsa.GenerateKey(rand.Reader, mldsa.MLDSA87)
+			if err != nil {
+				return nil, fmt.Errorf("failed to generate ML-DSA-87 key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
 		}
-		s.publicKey = pubKey
-		s.address = deriveAddress(pubKey)
+		s.address = deriveAddress(s.publicKey)
 
 	case walletcrypto.MLDSA65:
-		pubKey, err := mldsa.GeneratePublicKey65(privateKeyBytes)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate ML-DSA-65 public key: %w", err)
+		if len(privateKeyBytes) > 0 {
+			privKey, err := mldsa.PrivateKeyFromBytes(mldsa.MLDSA65, privateKeyBytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load ML-DSA-65 private key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
+		} else {
+			privKey, err := mldsa.GenerateKey(rand.Reader, mldsa.MLDSA65)
+			if err != nil {
+				return nil, fmt.Errorf("failed to generate ML-DSA-65 key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
 		}
-		s.publicKey = pubKey
-		s.address = deriveAddress(pubKey)
+		s.address = deriveAddress(s.publicKey)
 
 	case walletcrypto.MLDSA44:
-		pubKey, err := mldsa.GeneratePublicKey44(privateKeyBytes)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate ML-DSA-44 public key: %w", err)
+		if len(privateKeyBytes) > 0 {
+			privKey, err := mldsa.PrivateKeyFromBytes(mldsa.MLDSA44, privateKeyBytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load ML-DSA-44 private key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
+		} else {
+			privKey, err := mldsa.GenerateKey(rand.Reader, mldsa.MLDSA44)
+			if err != nil {
+				return nil, fmt.Errorf("failed to generate ML-DSA-44 key: %w", err)
+			}
+			s.mldsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
 		}
-		s.publicKey = pubKey
-		s.address = deriveAddress(pubKey)
+		s.address = deriveAddress(s.publicKey)
 
 	case walletcrypto.SLHDSA256:
-		pubKey, err := slhdsa.GeneratePublicKey256(privateKeyBytes)
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate SLH-DSA-256 public key: %w", err)
+		if len(privateKeyBytes) > 0 {
+			privKey, err := slhdsa.PrivateKeyFromBytes(slhdsa.SHA2_256s, privateKeyBytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load SLH-DSA-256 private key: %w", err)
+			}
+			s.slhdsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
+		} else {
+			privKey, err := slhdsa.GenerateKey(rand.Reader, slhdsa.SHA2_256s)
+			if err != nil {
+				return nil, fmt.Errorf("failed to generate SLH-DSA-256 key: %w", err)
+			}
+			s.slhdsaKey = privKey
+			s.publicKey = privKey.PublicKey.Bytes()
 		}
-		s.publicKey = pubKey
-		s.address = deriveAddress(pubKey)
+		s.address = deriveAddress(s.publicKey)
 
 	default:
 		return nil, fmt.Errorf("unsupported PQC scheme: %d", scheme)
@@ -71,14 +111,16 @@ func NewSigner(scheme walletcrypto.SignatureScheme, privateKeyBytes []byte) (wal
 
 func (s *signer) Sign(msg []byte) ([]byte, error) {
 	switch s.scheme {
-	case walletcrypto.MLDSA87:
-		return mldsa.Sign87(s.privateKey, msg)
-	case walletcrypto.MLDSA65:
-		return mldsa.Sign65(s.privateKey, msg)
-	case walletcrypto.MLDSA44:
-		return mldsa.Sign44(s.privateKey, msg)
+	case walletcrypto.MLDSA87, walletcrypto.MLDSA65, walletcrypto.MLDSA44:
+		if s.mldsaKey == nil {
+			return nil, fmt.Errorf("ML-DSA key not initialized")
+		}
+		return s.mldsaKey.Sign(rand.Reader, msg, nil)
 	case walletcrypto.SLHDSA256:
-		return slhdsa.Sign256(s.privateKey, msg)
+		if s.slhdsaKey == nil {
+			return nil, fmt.Errorf("SLH-DSA key not initialized")
+		}
+		return s.slhdsaKey.Sign(rand.Reader, msg, nil)
 	default:
 		return nil, fmt.Errorf("unsupported scheme: %d", s.scheme)
 	}
@@ -98,14 +140,16 @@ func (s *signer) Scheme() walletcrypto.SignatureScheme {
 
 func (s *signer) Verify(msg, sig []byte) bool {
 	switch s.scheme {
-	case walletcrypto.MLDSA87:
-		return mldsa.Verify87(s.publicKey, msg, sig)
-	case walletcrypto.MLDSA65:
-		return mldsa.Verify65(s.publicKey, msg, sig)
-	case walletcrypto.MLDSA44:
-		return mldsa.Verify44(s.publicKey, msg, sig)
+	case walletcrypto.MLDSA87, walletcrypto.MLDSA65, walletcrypto.MLDSA44:
+		if s.mldsaKey == nil || s.mldsaKey.PublicKey == nil {
+			return false
+		}
+		return s.mldsaKey.PublicKey.Verify(msg, sig, nil)
 	case walletcrypto.SLHDSA256:
-		return slhdsa.Verify256(s.publicKey, msg, sig)
+		if s.slhdsaKey == nil || s.slhdsaKey.PublicKey == nil {
+			return false
+		}
+		return s.slhdsaKey.PublicKey.Verify(msg, sig, nil)
 	default:
 		return false
 	}
@@ -116,6 +160,6 @@ func (s *signer) Verify(msg, sig []byte) bool {
 func deriveAddress(publicKey []byte) ids.ShortID {
 	// Hash the public key to create address
 	// This ensures PQC addresses are the same format as classic
-	hashed := ids.ToShortID(ids.HashBytes(publicKey))
-	return hashed
+	shortID, _ := ids.ToShortID(publicKey)
+	return shortID
 }
