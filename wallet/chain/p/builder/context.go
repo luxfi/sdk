e1f1addab8410c153462210eb8167b814f8a1013
Zach Kelling <z@zeekay.io>
2025-12-04 16:09:46 -0800

    Add unified wallet with PQC support
    - Created wallet/ directory structure for all chains
    - Moved P/X/C chain wallets from node to SDK
    - Added crypto abstraction layer (classic + PQC)
    - Support for ML-DSA, ML-KEM, SLH-DSA post-quantum crypto
    - Unified Signer interface works across all chains
    - Ready for Zoo and other EVM deployments
    
    Architecture:
    - wallet/crypto/{classic,pqc} - Crypto implementations
    - wallet/chain/{p,x,c,evm} - Chain-specific wallets
    - wallet/primary - Unified multi-chain wallet

---
+++ wallet/chain/p/builder/context.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package builder
+
+import (
+	consensusctx "github.com/luxfi/consensus/context"
+	"github.com/luxfi/ids"
+	"github.com/luxfi/node/utils/constants"
+	"github.com/luxfi/node/vms/components/gas"
+	"github.com/luxfi/node/vms/platformvm/txs/fee"
+)
+
+const Alias = "P"
+
+type Context struct {
+	NetworkID         uint32
+	ChainID           ids.ID // Optional: if set, overrides default PlatformChainID
+	XAssetID          ids.ID
+	ComplexityWeights gas.Dimensions
+	GasPrice          gas.Price
+	StaticFeeConfig   fee.StaticConfig
+}
+
+func NewConsensusContext(networkID uint32, luxAssetID ids.ID) (*consensusctx.Context, error) {
+	return NewConsensusContextWithChainID(networkID, constants.PlatformChainID, luxAssetID)
+}
+
+func NewConsensusContextWithChainID(networkID uint32, chainID ids.ID, luxAssetID ids.ID) (*consensusctx.Context, error) {
+	lookup := ids.NewAliaser()
+	ctx := &consensusctx.Context{
+		NetworkID:   networkID,
+		NetID:    constants.PrimaryNetworkID,
+		ChainID:     chainID,
+		XAssetID: luxAssetID,
+	}
+	return ctx, lookup.Alias(chainID, Alias)
+}
---
+++ wallet/chain/p/wallet/backend_visitor.go
@@ -0,0 +1,183 @@
+// Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package wallet
+
+import (
+	"context"
+	"errors"
+
+	"github.com/luxfi/ids"
+	"github.com/luxfi/node/utils/constants"
+	"github.com/luxfi/node/vms/components/lux"
+	"github.com/luxfi/node/vms/platformvm/txs"
+	"github.com/luxfi/node/vms/platformvm/warp"
+	"github.com/luxfi/node/vms/platformvm/warp/message"
+	"github.com/luxfi/node/vms/platformvm/warp/payload"
+	"github.com/luxfi/node/vms/secp256k1fx"
+)
+
+var (
+	_ txs.Visitor = (*backendVisitor)(nil)
+
+	ErrUnsupportedTxType = errors.New("unsupported tx type")
+)
+
+// backendVisitor handles accepting of transactions for the backend
+type backendVisitor struct {
+	b    *backend
+	ctx  context.Context
+	txID ids.ID
+}
+
+func (*backendVisitor) AdvanceTimeTx(*txs.AdvanceTimeTx) error {
+	return ErrUnsupportedTxType
+}
+
+func (*backendVisitor) RewardValidatorTx(*txs.RewardValidatorTx) error {
+	return ErrUnsupportedTxType
+}
+
+func (b *backendVisitor) AddValidatorTx(tx *txs.AddValidatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) AddNetValidatorTx(tx *txs.AddNetValidatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) AddDelegatorTx(tx *txs.AddDelegatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) CreateChainTx(tx *txs.CreateChainTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) CreateNetTx(tx *txs.CreateNetTx) error {
+	b.b.setOwner(
+		b.txID,
+		tx.Owner,
+	)
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) ImportTx(tx *txs.ImportTx) error {
+	err := b.b.removeUTXOs(
+		b.ctx,
+		tx.SourceChain,
+		tx.InputUTXOs(),
+	)
+	if err != nil {
+		return err
+	}
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) ExportTx(tx *txs.ExportTx) error {
+	for i, out := range tx.ExportedOutputs {
+		err := b.b.AddUTXO(
+			b.ctx,
+			tx.DestinationChain,
+			&lux.UTXO{
+				UTXOID: lux.UTXOID{
+					TxID:        b.txID,
+					OutputIndex: uint32(len(tx.Outs) + i),
+				},
+				Asset: lux.Asset{ID: out.AssetID()},
+				Out:   out.Out,
+			},
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) RemoveNetValidatorTx(tx *txs.RemoveNetValidatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) TransformNetTx(tx *txs.TransformNetTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) AddPermissionlessValidatorTx(tx *txs.AddPermissionlessValidatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) AddPermissionlessDelegatorTx(tx *txs.AddPermissionlessDelegatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) TransferNetOwnershipTx(tx *txs.TransferNetOwnershipTx) error {
+	b.b.setOwner(
+		tx.Net,
+		tx.Owner,
+	)
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) BaseTx(tx *txs.BaseTx) error {
+	return b.baseTx(tx)
+}
+
+func (b *backendVisitor) ConvertNetToL1Tx(tx *txs.ConvertNetToL1Tx) error {
+	for i, vdr := range tx.Validators {
+		b.b.setOwner(
+			tx.Net.Append(uint32(i)),
+			&secp256k1fx.OutputOwners{
+				Threshold: vdr.DeactivationOwner.Threshold,
+				Addrs:     vdr.DeactivationOwner.Addresses,
+			},
+		)
+	}
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) RegisterL1ValidatorTx(tx *txs.RegisterL1ValidatorTx) error {
+	warpMessage, err := warp.ParseMessage(tx.Message)
+	if err != nil {
+		return err
+	}
+	addressedCallPayload, err := payload.ParseAddressedCall(warpMessage.Payload)
+	if err != nil {
+		return err
+	}
+	registerL1ValidatorMessage, err := message.ParseRegisterL1Validator(addressedCallPayload.Payload)
+	if err != nil {
+		return err
+	}
+
+	b.b.setOwner(
+		registerL1ValidatorMessage.ValidationID(),
+		&secp256k1fx.OutputOwners{
+			Threshold: registerL1ValidatorMessage.DisableOwner.Threshold,
+			Addrs:     registerL1ValidatorMessage.DisableOwner.Addresses,
+		},
+	)
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) SetL1ValidatorWeightTx(tx *txs.SetL1ValidatorWeightTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) IncreaseL1ValidatorBalanceTx(tx *txs.IncreaseL1ValidatorBalanceTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) DisableL1ValidatorTx(tx *txs.DisableL1ValidatorTx) error {
+	return b.baseTx(&tx.BaseTx)
+}
+
+func (b *backendVisitor) baseTx(tx *txs.BaseTx) error {
+	return b.b.removeUTXOs(
+		b.ctx,
+		constants.PlatformChainID,
+		tx.InputIDs(),
+	)
+}
+
+// Duplicate methods removed - already defined above
