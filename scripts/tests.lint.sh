354ef91aa71d9f3ea33208ce94bd9271624df4d4
Zach Kelling <z@zeekay.ai>
2026-01-01 21:27:44 -0800

    fix(lint): update static analysis script to golangci-lint v2

--- scripts/tests.lint.sh
+++ scripts/tests.lint.sh
@@ -28,7 +28,7 @@ TESTS=${TESTS:-"golangci_lint license_header"}
 
 # https://github.com/golangci/golangci-lint/releases
 function test_golangci_lint {
-  go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.6
+  go install -v github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.6
   golangci-lint run --config .golangci.yml
 }
 
